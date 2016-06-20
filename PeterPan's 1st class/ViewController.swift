318d3d450d41c0ee58468f394f317d824eceb2b5
Fly <imbearfly@efgpdigiwinde-macbook-air.local>
2016-06-20 19:34:32 +0800

    Initial Commit

---
+++ PeterPan's 1st class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeterPan's 1st class
+//
+//  Created by Fly on 2016/6/20.
+//  Copyright © 2016年 Fly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
